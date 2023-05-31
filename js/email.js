8307455d55391a76ba4bc8c790d9e6ec261da1a1
carloeusebi <carloeusebi@gmail.com>
2023-05-31 14:31:48 +0200

    Read and validate email

--- js/email.js
+++ js/email.js
@@ -5,10 +5,25 @@ const emailError = document.getElementById('email-error-message');
 const emailInvalid = document.getElementById('email-invalid-alert');
 const emailAlertButton = document.getElementById('alert-button');
 
+// create allowed emails array
 const allowedEmails = ['carloeusebi@boolean.it',
     'mariusminia@boolean.it',
     'marcobaroncini@boolean.it',
     'marcolancellotti@boolean.it'
 ];
 
-console.table(allowedEmails);
+emailButton.addEventListener('click', function () {
+
+    // clean old errors
+    emailElement.classList.remove('is-invalid');
+    emailError.style.display = 'none';
+
+    const userEmail = emailElement.value.trim();
+
+    // validation
+    if (!userEmail) {
+        emailElement.classList.add('is-invalid');
+        emailError.style.display = 'block';
+        return;
+    }
+})
