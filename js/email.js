03acaa4f6c201ebd25cd72331d89ff4dd5473173
carloeusebi <carloeusebi@gmail.com>
2023-05-31 14:58:26 +0200

    Added comparison results

--- js/email.js
+++ js/email.js
@@ -3,7 +3,8 @@ const emailElement = document.getElementById('user-email');
 const emailButton = document.getElementById('btn-email');
 const emailError = document.getElementById('email-error-message');
 const emailInvalid = document.getElementById('email-invalid-alert');
-const emailAlertButton = document.getElementById('alert-button');
+
+const playButton = document.getElementById('play-button');
 
 // create allowed emails array
 const allowedEmails = ['carloeusebi@boolean.it',
@@ -17,21 +18,49 @@ emailButton.addEventListener('click', function () {
     // clean old errors
     emailElement.classList.remove('is-invalid');
     emailError.style.display = 'none';
+    emailInvalid.classList.add('d-none');
 
+    // read user input
     const userEmail = emailElement.value.trim();
 
     // validation
     if (!userEmail) {
         emailElement.classList.add('is-invalid');
         emailError.style.display = 'block';
+        playButton.classList.add('d-none');
         return;
     }
 
     let isAllowed;
+    let numberOfIteration = 0;
 
+    /* 
+    FOR cicle exit conditions:
+        - if a match is found we exit immediatly
+        - if a match was not found and we iterated throu all the array length
+     */
     for (let i = 0; !isAllowed && i < allowedEmails.length; i++) {
+
+        numberOfIteration++;
+
         if (userEmail === allowedEmails[i]) isAllowed = true;
-        console.log(i);
-        console.log(isAllowed);
+
     }
+
+    // console log the number of iterations for debug purposes
+    console.log('Number of iterations: ' + numberOfIteration);
+
+    // if email address is allowed show the play button;
+    // otherwise show the 'not authorized' alert and hide the button if it was on display
+    if (isAllowed) {
+
+        playButton.classList.remove('d-none');
+
+    } else {
+
+        playButton.classList.add('d-none');
+        emailInvalid.classList.remove('d-none');
+
+    }
+
 })
