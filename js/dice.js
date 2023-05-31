a170461ddb351d23bf105a9765e45d7faf07b690
carloeusebi <carloeusebi@gmail.com>
2023-05-31 15:25:46 +0200

    Added roll function and const for dom elements

--- js/dice.js
+++ js/dice.js
@@ -1,1 +1,17 @@
-console.log('dice.js ok!')
+//function to generate rnd numbers
+function rollDice() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+const playButton = document.getElementById('play-button');
+const displayResult = document.getElementById('result-message');
+
+playButton.addEventListener('click', function () {
+    // throw dice for player and cpu
+    const playerDice = rollDice();
+    const cpuDice = rollDice();
+
+    console.log('Player roll: ' + playerDice);
+    console.log('Cpu roll: ' + cpuDice);
+    console.log('--------------------------')
+})
