b4f07326bf690fbefd6fdeca1c37d0c4a42f0da0
carloeusebi <carloeusebi@gmail.com>
2023-05-31 14:18:37 +0200

    Targeted dom elements

--- js/email.js
+++ js/email.js
@@ -1,1 +1,12 @@
-console.log('email.js ok!');
+// get DOM elements
+const emailElement = document.getElementById('user-email');
+const emailButton = document.getElementById('btn-email');
+const emailError = document.getElementById('email-error-message');
+const emailInvalid = document.getElementById('email-invalid-alert');
+const emailAlertButton = document.getElementById('alert-button');
+
+console.log(emailElement)
+console.log(emailButton)
+console.log(emailError)
+console.log(emailInvalid)
+console.log(emailAlertButton)
